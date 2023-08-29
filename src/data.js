00a33c86698323849bbd39016244f6b8146d9d00
Sumit-Bhardwaj-BPIT <sumitbh720@gmail.com>
2023-08-29 23:54:10 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Bill Gates",
+    job: "Microsoft",
+    image: "https://th.bing.com/th/id/OIP.87SZFcMClMkg-aVCyWOlnQHaEK?w=294&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    text: "William Henry Gates III is an American business magnate, investor, and philanthropist best known for co-founding the software giant Microsoft along with his childhood friend Paul Allen. During his career at Microsoft, Gates held the positions of chairman, chief executive officer, president, and chief software architect, while also being its largest individual shareholder until May 2014. He was a major entrepreneur of the microcomputer revolution of the 1970s and 1980s.",
+  },
+  {
+    id: 2,
+    name: "Elon Musk",
+    job: "Tesla/SpaceX",
+    image: "https://th.bing.com/th/id/OIP.fgRXWPKByzkVbLRuOt8bxgHaFj?w=238&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    text: "Elon Musk is a South African-born American billionaire and entrepreneur who co-founded PayPal, SpaceX, Tesla, and Meta. He was born in 1971 and moved to Canada at 18. He studied physics and economics at the University of Pennsylvania and dropped out of Stanford University. He is the founder, CEO, and chief engineer of SpaceX, a spaceflight company that aims to colonize Mars. He is also the CEO and product architect of Tesla.",
+  },
+  {
+    id: 3,
+    name: "Mark Zuckerburg",
+    job: "Meta",
+    image: "https://th.bing.com/th/id/OIP.An3_-H4XOizqvgP6Kj_GRQHaE7?w=291&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    text: "Mark Zuckerberg is an American computer programmer and business magnate who co-founded Meta (formerly Facebook), a social networking website. He started Facebook in 2004 at Harvard University with his roommates and later became its CEO and executive chairman. He also co-founded the Chan Zuckerberg Initiative, a philanthropic organization. ",
+  },
+  {
+    id: 4,
+    name: "Steve Jobs",
+    job: "Apple",
+    image: "https://th.bing.com/th/id/OIP.rSIdNpBlZTai8nxXJtgJ_wHaEc?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    text: "Steve Jobs was an American inventor, designer, and entrepreneur who co-founded Apple Inc. and Pixar Animation Studios. He was born in 1955 and adopted by a couple in California. He dropped out of college and started Apple with his friend Steve Wozniak in 1976. He revolutionized the personal computer industry with products like the Macintosh, iPod, iPhone, and iPad. ",
+  },
+  {
+    id: 5,
+    name: "Larry Page",
+    job: "Google",
+    image: "https://th.bing.com/th/id/OIP.CN6GLe7ance3l3oyNBVENQHaE8?w=268&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    text: "Larry Page is an American billionaire and internet entrepreneur who co-founded Google with Sergey Brin. He was Google’s CEO from 1997 to 2001 and from 2011 to 2015. He then became CEO of Alphabet Inc., Google’s parent company. He is also the co-creator of PageRank, a search ranking algorithm. He has invested in flying car startups Kitty Hawk and Opener. As of June 2023, he is the eighth-richest person in the world with a net worth of $112 billion.",
+  },
+];
+
+export default reviews;
